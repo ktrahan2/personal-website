b29de8017e59a06f6f97e0a70b0c5d25870c8aac
Kyle Trahan <ktrahan2@gmail.com>
2020-11-25 11:23:53 -0500

    added transition rotation

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,6 +4,8 @@ export default function Home() {
 
     const [ isActiveQuadrant, setActiveQuadrant ] = useState("")
 
+    const quadrantArray = ["who-i-am", "projects", "hobbies", "contact-information"]
+
     return (
         <>
             <header>
@@ -13,54 +15,96 @@ export default function Home() {
             </header>
             <main>
                 <div 
-                    className={isActiveQuadrant === "who-i-am" ? "quadrant expand-quadrant" : "quadrant"} 
+                    className={isActiveQuadrant === "who-i-am" ? 
+                        "quadrant expand-quadrant" 
+                        : "quadrant"
+                    } 
                     id="top-left-div"
                 >
                     <div 
-                        className={isActiveQuadrant === "who-i-am" ? "post-container expand-post-container" : "post-container"} 
+                        className={isActiveQuadrant === "who-i-am" ? 
+                            "post-container expand-post-container" 
+                            : "post-container rotate-left"
+                        } 
                         id="who-i-am"
-                        onClick={ () => setActiveQuadrant("who-i-am")}
-                        
+                        onClick={ 
+                            isActiveQuadrant === "who-i-am" ? 
+                                () => setActiveQuadrant("") 
+                            :
+                                () => setActiveQuadrant("who-i-am")
+                        }
                     >
                         <h2 className="post-title" >Who I Am</h2>
                         <p className="post-information">scribble scrabble</p>
                     </div>
                 </div>
                 <div 
-                    className={isActiveQuadrant === "projects" ? "quadrant expand-quadrant" : "quadrant"} 
+                    className={isActiveQuadrant === "projects" ? 
+                        "quadrant expand-quadrant" 
+                        : "quadrant"
+                    } 
                     id="top-right-div"
                 >
                     <div 
-                        className={isActiveQuadrant === "projects" ? "post-container expand-post-container" : "post-container"} 
+                        className={isActiveQuadrant === "projects" ? 
+                            "post-container expand-post-container" 
+                            : "post-container rotate-right"
+                        } 
                         id="projects"
-                        onClick={ () => setActiveQuadrant("projects")}
+                        onClick={ 
+                            isActiveQuadrant === "projects" ? 
+                                () => setActiveQuadrant("") 
+                            :
+                                () => setActiveQuadrant("projects")
+                        }
                     >
                         <h2 className="post-title" >Projects</h2>
                         <p className="post-information"></p>
                     </div>
                 </div>
                 <div 
-                    className={isActiveQuadrant === "hobbies" ? "quadrant expand-quadrant" : "quadrant"} 
+                    className={isActiveQuadrant === "hobbies" ? 
+                        "quadrant expand-quadrant" 
+                        : "quadrant"
+                    } 
                     id="bottom-left-div"
                 >
                     <div 
-                        className={isActiveQuadrant === "hobbies" ? "post-container expand-post-container" : "post-container"} 
+                        className={isActiveQuadrant === "hobbies" ? 
+                            "post-container expand-post-container" 
+                            : "post-container rotate-left"
+                        } 
                         id="hobbies"
-                        onClick={ () => setActiveQuadrant("hobbies")}
+                        onClick={ 
+                            isActiveQuadrant === "hobbies" ? 
+                                () => setActiveQuadrant("") 
+                            :
+                                () => setActiveQuadrant("hobbies")
+                        }
                     >
                         <h2 className="post-title">Hobbies</h2>
                         <p className="post-information"></p>
                     </div>
                 </div>
                 <div 
-                        className={isActiveQuadrant === "contact-information" ? "quadrant expand-quadrant" : "quadrant"} 
+                        className={ isActiveQuadrant === "contact-information" ? 
+                            "quadrant expand-quadrant" 
+                            : "quadrant"
+                        } 
                         id="bottom-right-div"
                 >
                     <div 
-                        className={isActiveQuadrant === "contact-information" ? "post-container expand-post-container" : "post-container"} 
+                        className={ isActiveQuadrant === "contact-information" ? 
+                            "post-container expand-post-container" 
+                            : "post-container rotate-right"
+                        } 
                         id="contact-information"
-                        onClick={ () => setActiveQuadrant("contact-information")}
-                    >
+                        onClick={ 
+                            isActiveQuadrant === "contact-information" ? 
+                                () => setActiveQuadrant("") 
+                            :
+                                () => setActiveQuadrant("contact-information")
+                        }                    >
                         <h2 className="post-title">Contact</h2>
                         <p className="post-information"></p>
                     </div>
