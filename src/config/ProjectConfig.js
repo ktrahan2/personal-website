89319953d4ab5882368cc24a51a4af7567d9a395
Kyle Trahan <ktrahan2@gmail.com>
2021-01-05 13:20:53 -0700

    update project descriptions to fit on mobile

--- src/config/ProjectConfig.js
+++ src/config/ProjectConfig.js
@@ -2,13 +2,15 @@ export const projects =
 [
     {
         title: "On The Hunt",
-        description: "Whenever users first load into the app they will be greeted on the home page with a description of what the app is and an example of what a scavenger hunt might look like. From here the user can sign up or login and gain access to their previous saved hunts or create a new list from a random generator. While using their scavenger hunt list the user will be able to save their checked progress in case they aren't able to finish and want to come back to it later.",
+        description: "Whenever users first load into the app they will be greeted on the home page with a description of what the app is and an example of what a scavenger hunt might look like. From here the user can sign up or login and gain access to their previous saved hunts or create a new list from a random generator. While using their scavenger hunt list the user will be able to save their checked progress.",
+        // in case they aren't able to finish and want to come back to it later.",
         demo: "https://www.youtube.com/watch?v=RJUS6SivbOI&t=4s",
         github: "https://github.com/ktrahan2/scavenger-hunt-frontend",
     },
     {
         title: "Spacey Bois",
-        description: "Spacey Bois frontend is made with React-Redux and vanilla CSS. The game is based around a role-playing game called Scum and Villainy. It allows the user to create a character with a name and class type. Then the user is prompted with different options in order to advance the storyline. The storyline prompts are mostly generic but there is an example of one custom prompt showing what happens when a user makes an attack roll. At the end the player is given a score which is then add into the database and rendered on the /highscores route on the frontend.",
+        description: "Spacey Bois frontend is made with React-Redux and vanilla CSS. The game is based around a role-playing game called Scum and Villainy. It allows the user to create a character with a name and class type. Then the user is prompted with different options in order to advance the storyline.",
+        //The storyline prompts are mostly generic but there is an example of one custom prompt showing what happens when a user makes an attack roll. At the end the player is given a score which is then add into the database and rendered on the /highscores route on the frontend.",
         demo: "https://www.youtube.com/watch?v=xcrq1DzK1YA",
         github: "https://github.com/ktrahan2/spacey-bois-frontend",
     },
